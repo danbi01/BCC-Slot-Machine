3466fe4524a4d619ecfe8a2e171d858df1cd45c1
danbi01 <twobi01@gmail.com>
2024-11-05 22:10:16 +0900

    Change score scene animation timing

--- Assets/Scripts/Ribbon.cs
+++ Assets/Scripts/Ribbon.cs
@@ -6,20 +6,74 @@ using UnityEngine.EventSystems;
 
 public class Ribbon : MonoBehaviour
 {
+    public GameObject candle;
+    public GameObject lightEffect;
+    public GameObject cakeTopper;
+    public static bool isRibbonClicked;
+
+    int rank = 0;
+
     public void RibbonCickHandler()
     {
-        //GetComponent<RectTransform>().localScale= new Vector2(1.5f, 1.5f);
+        isRibbonClicked = true;
+        Debug.Log("isRibbonClicked: "+isRibbonClicked);
         Envelope.anim.Play("Envelope_open");
-        gameObject.SetActive(false);
+
+        // 리본 클릭했을 때 촛불, 효과 실행
+        StartCoroutine(setTimeOutClickRibbon());
+
+        GetComponent<Image>().enabled= false;
+    }
+
+    IEnumerator setTimeOutClickRibbon()
+    {
+        yield return new WaitForSeconds(0.5f); // 편지지 넣고 수정하기
+        candle.SetActive(true);
+        lightEffect.SetActive(true);
+        Debug.Log("Play candleLight");
+        try
+        {
+            if (GameManager.score == 100)
+            {
+                rank = 1;
+                CandleLight.rectTransform.anchoredPosition = new Vector2(138f, 131f);
+                LightEffect.rectTransform.anchoredPosition = new Vector2(138f, 103f);
+            }
+            else if (GameManager.score > 85)
+            {
+                rank = 2;
+                CandleLight.rectTransform.anchoredPosition = new Vector2(401.7f, 8.4f);
+                LightEffect.rectTransform.anchoredPosition = new Vector2(401.7f, -19.96f);
+            }
+            else if (GameManager.score > 75)
+            {
+                rank = 3;
+                CandleLight.rectTransform.anchoredPosition = new Vector2(624.3f, -67.8f);
+                LightEffect.rectTransform.anchoredPosition = new Vector2(624.3f, -100f);
+            }
+            else
+            {
+                rank = 4;
+                CandleLight.rectTransform.anchoredPosition = new Vector2(825.7f, -166f);
+                LightEffect.rectTransform.anchoredPosition = new Vector2(825.7f, -194f);
+                cakeTopper.SetActive(true);
+            }
+        } catch
+        {
+            StartCoroutine(setTimeOutClickRibbon());
+        }
     }
 
     void Start()
     {
         // 투명배경 무시
         GetComponent<Image>().alphaHitTestMinimumThreshold = 0.001f;
-    }
-    void Update()
-    {
 
+        isRibbonClicked = false;
+
+        candle.SetActive(false);
+        cakeTopper.SetActive(false);
+        lightEffect.SetActive(false);
     }
+
 }
--- Assets/Scripts/ScoreButton.cs
+++ Assets/Scripts/ScoreButton.cs
@@ -19,13 +19,7 @@ public class ScoreButton : MonoBehaviour
     public GameObject background;
     public GameObject door;
     public GameObject halfDoor;
-    public GameObject candle;
-    public GameObject cakeTopper;
-    public GameObject lightEffect;
     bool isScoreButtonClicked = false;
-    public static bool isLightOn = false;
-
-    int rank = 0;
 
     // 열쇠 클릭 시 열쇠, 문, 배경 비활성화
     public void ScoreButtonClickHandler()
@@ -53,47 +47,13 @@ public class ScoreButton : MonoBehaviour
         doorSound.Play(0);
         Debug.Log("Play doorOpen");
         
-        // 불 켜짐, 편지지 열림
+        // 불 켜짐, 캐릭터 표시
         yield return new WaitForSeconds(1.5f);
-        isLightOn = true;
         lightSwitchSound.Play(0);
         shalalaSound.Play(0);
         door.SetActive(false);
         background.SetActive(false);
         GameManager.AddSelectedParts();
-        //Envelope.anim.Play("Envelope_open");
-
-        // 촛불 켜짐
-        yield return new WaitForSeconds(1f);
-        candle.SetActive(true);
-        lightEffect.SetActive(true);
-        Debug.Log("Play candleLight");
-
-        // candle.SetActive(true) 기다리기
-        yield return new WaitForSeconds(0.00001f);
-        if (GameManager.score == 100) {
-            rank = 1;
-            CandleLight.rectTransform.anchoredPosition = new Vector2(138f, 131f); //138 131
-            LightEffect.rectTransform.anchoredPosition = new Vector2(138f, 103f);
-        }
-        else if(GameManager.score > 85) { // 95, 90
-            rank = 2;
-            CandleLight.rectTransform.anchoredPosition = new Vector2(401.7f, 8.4f);
-            LightEffect.rectTransform.anchoredPosition = new Vector2(401.7f, -19.96f);
-        }
-        else if(GameManager.score > 75) { // 85, 80, 75
-            rank = 3;
-            CandleLight.rectTransform.anchoredPosition = new Vector2(624.3f, -67.8f);//624.3, 67.8
-            LightEffect.rectTransform.anchoredPosition = new Vector2(624.3f, -100f);
-        } else {                         // 70 ... 0
-            rank = 4;
-            CandleLight.rectTransform.anchoredPosition = new Vector2(825.7f, -166f); // 166.2
-            LightEffect.rectTransform.anchoredPosition = new Vector2(825.7f, -194f);
-            cakeTopper.SetActive(true);
-        }
-        // 점수 출력
-        Debug.Log("Score: " + GameManager.score + ", Rank: " + rank +"등");
-
     }
 
     void Start()
@@ -113,9 +73,6 @@ public class ScoreButton : MonoBehaviour
         door.SetActive(true);
         halfDoor.SetActive(true);
         gameObject.SetActive(true);
-        candle.SetActive(false);
-        cakeTopper.SetActive(false);
-        lightEffect.SetActive(false);
 
         targetPosition = new Vector3(-1, transform.position.y, 0);
     }
@@ -127,6 +84,6 @@ public class ScoreButton : MonoBehaviour
         if (Input.GetKeyDown(KeyCode.Space)) {
             Debug.Log("스페이스 키 누름");
             SceneManager.LoadScene("Start Scene");
-        }
+        }       
     }
 }
